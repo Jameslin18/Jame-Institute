989b89f38d1ae28352f3dca446aa31b053ad1c06
Jame <jameslin18professional@gmail.com>
2022-03-30 22:10:57 -0400

    added delete file command

--- git pull.py
+++ git pull.py
@@ -3,8 +3,12 @@ import time
 
 os.system("cd Desktop/")
 time.sleep(1)
+os.system("sudo rm -r Jame-Institute")
+time.sleep(1)
 os.system("git clone https://github.com/Jameslin18/Jame-Institute.git")
 time.sleep(1)
 os.system("Jameslin18")
 time.sleep(1)
 os.system("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")
+time.sleep(1)
+break
