311c2e9a429fb1f4fc0e324fa471abb44ebe5d7b
Jame <jameslin18professional@gmail.com>
2022-04-07 10:22:44 -0400

    update git pull and add frequency

--- git pull.py
+++ git pull.py
@@ -1,11 +1,10 @@
 import os
 import time
-print("Jameslin18")
-print("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")
+#print("Jameslin18")
+#print("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")
 os.chdir("/home/jame/Desktop")
 time.sleep(1)
 os.system("sudo rm -r Jame-Institute")
 time.sleep(1)
 os.system("git clone https://github.com/Jameslin18/Jame-Institute.git")
 time.sleep(1)
-os.chdir("/home/jame/Desktop")
