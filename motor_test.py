95553777fa7fee416ee021e520dae5963a80dfa5
Jame <jameslin18professional@gmail.com>
2022-04-15 17:40:44 -0400

    change arm

--- motor_test.py
+++ motor_test.py
@@ -166,6 +166,8 @@ def arm():  # This is the arming procedure of an ESC
         pi.set_servo_pulsewidth(motor_1, max_value)
         time.sleep(1)
         pi.set_servo_pulsewidth(motor_1, min_value)
+        time.sleep(1)
+        pi.set_servo_pulsewidth(motor_1, 0)
         manual_drive()
 
 
