e03312c5759c1ad13f9eb07ce66802a6f636139e
Jame <jameslin18professional@gmail.com>
2022-04-22 10:55:00 -0400

    chose base wheel

--- calc.py
+++ calc.py
@@ -31,18 +31,33 @@ def power_set():
         except ValueError:
             print("cringe.")
 
+    wheel_throttle = float(2000 * float(power/100))
     print("Power set to ", power)
 
-    return power
+    return wheel_throttle
 
 
-# def choose_base_wheel():
-    # class WheelThrottle(object):
-        # def __init__(self):
-            # if 0 < theta
-
+def choose_base_wheel():
+    theta = angle_inp()
+    if 0 < float(theta) < 2 * math.pi / 3:
+        base = "left"
+        return base
+    elif 4 * math.pi / 3 < float(theta) < 2 * math.pi:
+        base = "right"
+        return base
+    elif 2 * math.pi / 3 < float(theta) < 4 * math.pi / 3:
+        base = "mid"
+        return base
+    elif float(theta) == 0:
+        base = "left+right"
+        return base
+    elif float(theta) == 2 * math.pi / 3:
+        base = "left+mid"
+        return base
 
 # power_set()
+
+w = 60
 
 
 def get_pulse():
@@ -50,17 +65,18 @@ def get_pulse():
     p = int(v / float(2000 / 9))
     return p
 
+
 def pulse_angular():
     pulse = int(raw_input())
     volt = float(float(9 / 2000) * pulse)
     wh_angular = float(volt * 60 * math.pi)
-    ball_angular = wh_angular * float(101.6 / 40)
-    return ball_angular
+    # ball_angular = wh_angular * float(101.6 / 40)
+    return wh_angular
 
 
 def angle_calc():
     while True:
-        class BallAngular(object):
+        class WheelAngular(object):
             def __init__(self):
                 while True:
                     try:
@@ -77,69 +93,66 @@ def angle_calc():
                     except ValueError:
                         print("cringe.")
 
-        ball = BallAngular()
+        wheel = WheelAngular()
 
         class LeftAngular:
-            i = ball.left * math.sin(math.pi / 6)  # left.i * sqrt3/2
-            j = ball.left * math.cos(math.pi / 6)  # left.j * 1/2
+            i = wheel.left * math.cos(4 * math.pi / 3)  # left.i * -1/2
+            j = wheel.left * math.sin(4 * math.pi / 3)  # left.j * -sqrt3/2
 
         l_vector = LeftAngular()
 
         class RightAngular:
-            i = ball.right * math.sin(11 * math.pi / 6)  # right.i * sqrt3/2
-            j = ball.right * math.cos(11 * math.pi / 6)  # right.i * -1/2
+            i = wheel.right * math.cos(2 * math.pi / 3)  # right.i * -1/2
+            j = wheel.right * math.sin(2 * math.pi / 3)  # right.i * sqrt3/2
 
         r_vector = RightAngular()
 
         class MidAngular:
-            i = ball.mid * math.cos(math.pi)  # mid.i * -1
-            j = ball.mid * math.sin(0)  # mid.i * 0
+            i = wheel.mid * math.cos(0)  # mid.i * 1
+            j = wheel.mid * math.sin(0)  # mid.i * 0
 
         m_vector = MidAngular()
 
         class NetAngular:
-            i = int(l_vector.i + r_vector.i + m_vector.i)  # (left.i * sqrt3/2) + (right.i * sqrt3/2) - mid.i
-            j = int(l_vector.j + r_vector.j + m_vector.j)  # (left.j * 1/2) - (right.i * 1/2) + 0
+            i = int(l_vector.i + r_vector.i + m_vector.i)  # mid.i -1/2(left.i) - 1/2(right.i)
+            j = int(l_vector.j + r_vector.j + m_vector.j)  # sqrt3/2(right.j) - sqrt3/2(left.i)
 
         n_vector = NetAngular()
 
-        try:
+        if n_vector.i == n_vector.j == 0:
+            print("\nNo spin.")
+        else:
             theta = math.atan2(n_vector.j, n_vector.i)
             deg = int((theta * float(180 / math.pi)))
             if deg < 0:
                 print("\nAngle = ", 360 + deg, "degrees")
             else:
                 print("\nAngle = ", deg, "degrees")
-            print("\n------------------")
-            print("i net = ", n_vector.i)
-            print("j net = ", n_vector.j)
-            print("------------------")
-            print("i left = ", l_vector.i)
-            print("i right = ", r_vector.i)
-            print("i mid = ", m_vector.i)
-            print("------------------")
-            print("j left = ", l_vector.j)
-            print("j right = ", r_vector.j)
-            print("j mid = ", m_vector.j)
-            print("------------------")
-            print("ball left = ", ball.left)
-            print("ball right = ", ball.right)
-            print("ball mid = ", ball.mid)
-            print("------------------")
-            print("\n")
 
-        except ZeroDivisionError:
-            print("no spin\n")
+        print("\nData:")
+        print("------------------")
+        print("i net = ", n_vector.i)
+        print("j net = ", n_vector.j)
+        print("------------------")
+        print("i left = ", l_vector.i)
+        print("i right = ", r_vector.i)
+        print("i mid = ", m_vector.i)
+        print("------------------")
+        print("j left = ", l_vector.j)
+        print("j right = ", r_vector.j)
+        print("j mid = ", m_vector.j)
+        print("------------------")
+        print("wheel left = ", wheel.left)
+        print("wheel right = ", wheel.right)
+        print("wheel mid = ", wheel.mid)
+        print("------------------")
+        print("\n")
 
         print("[stop] or any input.")
         inp = raw_input()
-
         if inp == "stop":
             break
 
 
 angle_calc()
 
-# deg = int(raw_input())
-
-# theta = float(deg * float(math.pi / 180))
