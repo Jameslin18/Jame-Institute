bf44b624e28b994aa009cc48d70edf30cbebaf0b
Jame <jameslin18professional@gmail.com>
2022-04-25 00:45:08 -0400

    solve test successful

--- solve_test.py
+++ solve_test.py
@@ -1,26 +1,15 @@
-from sympy import symbols
-import sympy as sy
+from sympy import Eq, symbols, solve
 import math
 
+
 theta = math.pi / 6
+k = float(math.tan(theta))
+w1 = 600
+wn = 3000
 
-tan = float(math.tan(theta))
+w2, w3 = symbols('w2 w3', real=True)
 
-ang_v = 600
+eq1 = Eq(2 * k * w3 + (math.sqrt(3) - k) * w1 - (math.sqrt(3) + k) * w2, 0)
+eq2 = Eq((w1 + w2) ** 2 + w3 * w3 - w1 * w3 - w2 * w3 - wn ** 2, 0)
 
-ang_n = 3000
-
-w1, wN, k, w2, w3 = sy.symbols('w1 wN k w2 w3')
-
-# f1 = Eq(2 * k * w3 + (math.sqrt(3) - k) * w1 - (math.sqrt(3) + k) * w2, 0)
-# f2 = Eq((w1 + w2) ** 2 + w3(w3 - w1 - w2) - wN ** 2, 0)
-
-sol = sy.nsolve((2 * k * w3 + (math.sqrt(3) - k) * w1 - (math.sqrt(3) + k) * w2),
-                ((w1 + w2) ** 2 + w3(w3 - w1 - w2) - wN ** 2),
-                (w2, w3), (0, 0))
-
-sy.nsolve.subs(w1, 600)
-sy.nsolve.subs(wN, 3000)
-sy.nsolve.subs(k, math.tan(math.pi/6))
-
-print(sol)
+print(solve([eq1, eq2]))
